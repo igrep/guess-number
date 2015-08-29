629fcb6403351b9dc4bec5159a309ccdf39baf0c
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-08-29 16:56:29 +0900

    first version: without any Monad except IO

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,67 @@
 module Lib
-    ( someFunc
-    ) where
+  ( playOnce
+  ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Monad (forM_)
+import System.IO (hSetBuffering, BufferMode(..), hSetEcho, stdout, stdin)
+import Control.Exception (bracket_)
+
+{-
+  - Setter: sets some numbers.
+  - Player: guesses the numbers Setter sets.
+-}
+
+guess
+  :: [Int] -- Numbers that's not matched yet.
+  -> Int   -- Number input by Player.
+  -> ([Ordering], [Int])
+guess leftNs inputN = (results, map snd onlyNonMatched)
+  where
+    nonMatched = (EQ /=) . fst
+    results = map (compare inputN) leftNs
+    onlyNonMatched = filter nonMatched (zip results leftNs)
+
+playOnce :: IO ()
+playOnce = do
+  hSetBuffering stdout NoBuffering
+  initialNs <- askNumbers
+  putStrLn ""
+  playOnceWith initialNs
+
+playOnceWith
+  :: [Int]  -- Inital numbers the Setter set.
+  -> IO () -- Count how many times Player guesses till all the numbers matches
+playOnceWith initialNs =
+  playOnceCountingFrom
+    1 -- this is the 1st play
+    initialNs
+
+playOnceCountingFrom :: Int -> [Int] -> IO ()
+playOnceCountingFrom count ns = do
+  putStr "Guess a number: "
+  input <- getLine
+  let (results, leftNs) = guess ns (read input)
+
+  forM_ results tellResult
+
+  if null leftNs
+    then do
+      putStrLn ""
+      putStrLn ("Congratulations! All numbers have matched!")
+      putStrLn ("You have guessed " ++ show count ++ " times.")
+    else do
+      putStrLn ("Left numbers: " ++ show (length leftNs))
+      playOnceCountingFrom (count + 1) leftNs
+
+tellResult :: Ordering -> IO ()
+tellResult EQ = putStrLn $ "  - Matched!"
+tellResult GT = putStrLn $ "  - Too large!"
+tellResult LT = putStrLn $ "  - Too small!"
+
+-- From http://stackoverflow.com/questions/4064378/prompting-for-a-password-in-haskell-command-line-application
+askNumbers :: IO [Int]
+askNumbers = do
+  putStr "Enter some numbers separated by space: "
+  s <- bracket_ (hSetEcho stdin False) (hSetEcho stdin True) getLine
+  putStrLn ""
+  return (map read (words s))
